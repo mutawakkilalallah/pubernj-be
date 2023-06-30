631d3336b15da366c620511fdf70524cd755ee2d
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2023-07-01 00:16:37 +0700

    update dropspot migration

--- migrations/20230630103917-create-dropspot.js
+++ migrations/20230630103917-create-dropspot.js
@@ -13,7 +13,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: "Area",
+          model: "area",
           key: "id",
         },
       },
