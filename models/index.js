04d71d456c23d19789c13b48218148fd0af01d26
mutawakkilalallah <mutawakkilalallah.19@gmail.com>
2024-03-22 02:54:40 +0700

    fix pool conn mysql

--- models/index.js
+++ models/index.js
@@ -9,45 +9,37 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-config.logging = false;
+// config.logging = false;
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     ...config,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 60000,
-      idle: 10000,
-    },
+    // pool: {
+    //   max: 10,
+    //   min: 0,
+    //   acquire: 60000,
+    //   idle: 10000,
+    // },
   });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
     ...config,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
+    // pool: {
+    //   max: 10,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000,
+    // },
   });
 }
 
 fs.readdirSync(__dirname)
   .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
-    );
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js" && file.indexOf(".test.js") === -1;
   })
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
